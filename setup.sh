173390db1306edd427e58b59e37f90a191db74d4
liminggui <linuxphp@126.com>
2020-06-06 20:10:25 +0800

    add user

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner 1000 -j RETURN
+sudo useradd -M -s /sbin/nologin anyproxy
+sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner anyproxy -j RETURN
 sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-port 3000
