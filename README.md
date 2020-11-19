60b572a43748e7c82dc2ac070313bee98757c01d
liminggui <linuxphp@126.com>
2020-11-19 18:51:08 +0800

    启动shell封装

--- README.md
+++ README.md
@@ -116,9 +116,10 @@ docker run  -p 3000:3000 anyproxy:latest -p '127.0.0.1:3001'
 sudo useradd -M -s /sbin/nologin anyproxy
 # uid为anyproxy的tcp请求不转发,并用anyproxy用户启动anyproxy程序
 sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner anyproxy -j RETURN
+# 单独指定root账号走代理
+sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT -m owner --uid-owner 0 --to-port 3000
 # 其它用户的tcp请求转发到本地3000端口
 sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-port 3000
-
 ```
 
 > 如果删除全局代理
@@ -166,6 +167,7 @@ sudo iptables -t nat -D OUTPUT 2
 * ~~支持HTTP/1.1 keep-alive 一外链接多次请求不同域名~~
 * HTTP/1.1 keep-alive后端也能复用tcp
 * ~~修复iptables转发后百度贴吧无法访问的问题~~
+* 转发的mysql的连接请求会一直卡住
 
 # 感谢
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,25 @@ SCRIPT=$(readlink -f $0)
 ROOT_DIR=$(dirname $SCRIPT)/../
 cd $ROOT_DIR
 
-sudo useradd -M -s /sbin/nologin anyproxy
-sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner anyproxy -j RETURN
-sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-port 3000
+if ! (sudo cat /etc/passwd|grep ^anyproxy: > /dev/null); then
+    echo "添加账号"
+    sudo useradd -M -s /sbin/nologin anyproxy
+fi
 
-sudo -u anyproxy ./anyproxy -daemon
+# 要启动的端口
+port=3000
+if ! (sudo iptables -t nat -L|grep "redir ports $port" > /dev/null); then
+    echo "添加iptables"
+    # anyproxy 账号不走代理直接请求
+    sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner anyproxy -j RETURN
+
+    # 注:如果有虚拟机，虚拟机的启动账号不要走代理端口，会导致虚拟机里的浏览器设置的代理无效果
+    #    如果有本地账号要连mysql等服务，也不要走代理端口,会一直卡住(目前发现mysql协议不兼容)
+
+    #指定root账号走代理
+    sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT -m owner --uid-owner 0 --to-port $port
+    #其它账号可以走代理
+    #sudo iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-port $port
+fi
+echo "启动anyproxy"
+sudo -u anyproxy ./anyproxy -daemon -l $port
